559aa43ec7f1de13e77d77e29f817dcfb133a63a
Nazareno Alves da Costa <nazarenoalvesdacosta@gmail.com>
2022-07-21 19:35:45 -0300

    exercicios do dia 2 de 1 ao 7

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -0,0 +1,66 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//Exercicio 1
+/*for(let index = 0;index < numbers.length; index++ ){
+    console.log(numbers[index]);
+}*/
+
+//2 soma de todos os elementos
+/*let soma = 0;
+for(let index = 0; index < numbers.length; index++){
+    soma += numbers[index];
+    console.log(soma);
+}*/
+
+//Exercicio 3
+
+/*let soma = 0;
+let media = 0;
+for(let index = 0; index < numbers.length; index++){
+    soma += numbers[index];
+  }
+  media = soma / 10;
+  console.log(media);*/
+
+//exercicio 4
+
+/*let soma = 0;
+let media = 0;
+for(let index = 0; index < numbers.length; index++){
+    soma += numbers[index];
+  }
+  media = soma / 10;
+  if(media > 20){
+    console.log('Valor maior que 20');
+  }
+  else{
+    console.log('Valor menor que 20');
+  }*/
+
+  //Exercicio 5
+ /*  let maior = 0;
+  for(let index =0; index < numbers.length; index++){
+    if(numbers[index] > maior ){
+        maior = numbers[index];
+    }
+  }
+  console.log(maior);*/
+// exercicio 6
+
+ /*  let resultado = 0;
+  for(let index =0; index < numbers.length; index++){
+    if(numbers[index] % 2 === 1){
+        resultado++
+    }
+  }console.log(resultado);*/
+
+  //menor valor
+ /*   let menor = Infinity;
+  for(let index =0; index < numbers.length; index++){
+    if(numbers[index] < menor ){
+        menor = numbers[index];
+    }
+  }
+  console.log(menor);*/
+
+
