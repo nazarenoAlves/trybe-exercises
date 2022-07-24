3a314fe26da5e9951a137343834c404727a11933
Nazareno Alves da Costa <nazarenoalvesdacosta@gmail.com>
2022-07-24 10:37:43 -0300

    Exercicios 1 e 2 dia 3

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -0,0 +1,14 @@
+ /* let numF = 10 ;
+  let resultado =1 ;
+  for(let index = 1; index <= numF; index++){
+    resultado *= index;
+    console.log(resultado);
+  }*/
+
+ /* let word = 'tryber';
+  let resultado = '';
+  for(let index = word.length - 1; index >= 0; index -= 1){
+    resultado += word[index];
+    
+    }
+    console.log(resultado);*/
