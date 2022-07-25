5ec4aca69aea3c3db1035629081a5cdcd75851be
Nazareno Alves da Costa <nazarenoalvesdacosta@gmail.com>
2022-07-25 18:46:58 -0300

    exercicios 2 a 4

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -1,7 +1,18 @@
-/*let info = {
+let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
   };
-
-  console.log('Bem-vinda ' + info.personagem);*/
+  //exercicio 1
+  console.log('Bem-vinda ' + info.personagem);
+  //exercicio 2
+  console.log(info.recorrente);
+  //exercicio 3
+  for(let key in info){
+    console.log(key);
+  }
+  //exercicio 4
+  for(let key in info){
+    console.log(info[key]);
+  }
