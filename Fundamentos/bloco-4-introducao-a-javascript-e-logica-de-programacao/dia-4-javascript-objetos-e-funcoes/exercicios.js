e98ea2103a1c4928f837c89a1f6ae78c74a928b8
Nazareno Alves da Costa <nazarenoalvesdacosta@gmail.com>
2022-07-25 22:05:26 -0300

    exercicios parte 1

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -4,7 +4,13 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
     recorrente: 'Sim'
   };
-  //exercicio 1
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain',
+  nota:'Dells Four Color Comics #178 O último MacPatinhas',
+  recorrente: 'sim',
+}  
+ /* //exercicio 1
   console.log('Bem-vinda ' + info.personagem);
   //exercicio 2
   console.log(info.recorrente);
@@ -15,4 +21,31 @@ let info = {
   //exercicio 4
   for(let key in info){
     console.log(info[key]);
-  }
+  }*/
+ /* 
+  console.log(info.personagem + ' e ' + info2.personagem);
+  console.log(info.origem + ' e ' + info2.origem);
+  console.log(info.nota + ' e ' + info2.nota);
+  console.log('Ambos recorrente ' + info.recorrente);*/
+
+  let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+      {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editora: 'Rocco',
+      }
+    ],
+  };
+  let livros = leitor.livrosFavoritos.length
+  console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' ' + 'se chama '+ leitor.livrosFavoritos[0].titulo);
+  console.log(leitor.nome + ' tem ' + livros + ' favoritos');
+  
