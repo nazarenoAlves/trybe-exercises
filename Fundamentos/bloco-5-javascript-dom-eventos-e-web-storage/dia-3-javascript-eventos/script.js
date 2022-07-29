82379e1f556c574989c410ba0021fccef06f2a07
Nazareno Alves da Costa <nazarenoalvesdacosta@gmail.com>
2022-07-29 19:09:26 -0300

    exercicio-1-ok

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -13,4 +13,21 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+ 
+
+ function addDays(){
+  let numberWeekDay = document.getElementById('days');
+  let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  
+  for(index = 0; index < decemberDaysList.length; index += 1){
+  const daysWeek  = decemberDaysList[index];
+  const addDays = document.createElement('li');
+  addDays.className = 'day';
+  addDays.innerHTML = daysWeek;
+
+  numberWeekDay.appendChild(addDays);
+  }
+ }
+
+ addDays();
+
