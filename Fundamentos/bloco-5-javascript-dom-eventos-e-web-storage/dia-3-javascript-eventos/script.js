70518cd5e6352b5e747b5f5057a67d2d57d55791
Nazareno Alves da Costa <nazarenoalvesdacosta@gmail.com>
2022-08-06 11:39:13 -0300

    exercicios 5.3

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -26,8 +26,74 @@ function createDaysOfTheWeek() {
   addDays.innerHTML = daysWeek;
 
   numberWeekDay.appendChild(addDays);
+
+  if(daysWeek === 24  || daysWeek === 31){
+    addDays.className = 'day holiday';
+  }else if (daysWeek === 4 || daysWeek === 11 || daysWeek === 18){
+    addDays.className = 'day friday';
+  }else if (daysWeek === 25){
+    addDays.className = 'day holiday friday';
+  }else{
+    addDays.className = 'day';
+  }
   }
  }
+ function createHolidayButton(buttonName) {
+  let buttonContainer = document.querySelector('.buttons-container')
+  let newButton = document.createElement('button');
+  let newButtonID = 'btn-holiday';
 
+  newButton.innerHTML = buttonName;
+  newButton.id = newButtonID;
+  buttonContainer.appendChild(newButton);
+}
+function displayButton(){
+  let getButtonHoliday = document.querySelector('#btn-holiday');
+  let getHoliday = document.querySelectorAll('.holiday');
+  let backgroundColor = 'rgb(238,238,238)';
+  let newColor = 'white';
+
+  getButtonHoliday.addEventListener('click', function(){
+  for(let index = 0; index < getHoliday.length; index += 1) {
+    if(getHoliday[index].style.backgroundColor === newColor){
+      getHoliday[index].style.backgroundColor = backgroundColor;
+    }else{
+      getHoliday[index].style.backgroundColor = newColor;
+    }
+  }
+  });
+}
+  function btnFriday(buttonName){
+    let buttonFriday = document.querySelector('.buttons-container');
+    let newButtonFriday = document.createElement('button')
+    let buttonId = 'btn-friday';
+
+    newButtonFriday.innerHTML = buttonName;
+    newButtonFriday.id = buttonId;
+    buttonFriday.appendChild(newButtonFriday);
+
+  }
+
+  function displayFriday(array){
+    let getButtonFriday = document.querySelector('#btn-friday');
+    let getFriday = document.querySelectorAll('.friday');
+    let newText = 'SEXTOU O/';
+
+    getButtonFriday.addEventListener('click', function(){
+      for(let index = 0; index < getFriday.length; index += 1){
+        if(getFriday[index].innerHTML !== newText){
+          getFriday.innerHTML = newText;
+          console.log(getFriday);
+        }else{
+          getFriday[index].innerHTML = array[index];
+        }
+      }
+    });
+  }
+ 
+ createHolidayButton('Feriados');
  addDays();
-
+ displayButton();
+ btnFriday('Sexta-Feira');
+ let decemberFridays = [ 4, 11, 18, 25 ];
+ displayFriday(decemberFridays);
