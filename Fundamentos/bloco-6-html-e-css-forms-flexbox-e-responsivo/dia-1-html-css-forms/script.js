4208da949f63a0df2d12fb244a16382ad816d34d
Nazareno Alves da Costa <nazarenoalvesdacosta@gmail.com>
2022-08-06 12:51:15 -0300

    exercicio 6.1

---
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,19 @@
+const nameUser = document.getElementById('name');
+const emailUser = document.getElementById('email');
+const localUser = document.getElementsByClassName('local');
+const textUser = document.getElementById('text');
+const dateUser = document.getElementById('date');
+const autorizeUser = document.getElementsByClassName('confirmNewsandImage');
+const submit = document.getElementById('enviar');
+const clearDisplay = document.getElementById('clear')
+
+function getUserValue(event){
+    event.preventDefault();
+    localStorage.setItem('nameUser', nameUser);
+    localStorage.setItem('emailUser', emailUser);
+    localStorage.setItem('localUser', localUser);
+    localStorage.setItem('textUser', textUser);
+    localStorage.setItem('dateUser', dateUser);
+    localStorage.setItem('autorizeUser', autorizeUser);
+}
+submit.addEventListener('click' getUserValue);
